023dcf3c1a9d8d4927be9f6f11b3e683f4e806bc
Tawfik-Tarek <tawfiktarek1975@gmail.com>
2024-06-07 00:23:24 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,55 @@
+const gameContainer = document.querySelector(".game");
+const allBlocks = document.querySelectorAll(".game .game-block");
+const wrongTries = document.querySelector(".info-container .tries span");
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector(".modal .close");
+const modalInput = document.querySelector(".modal input");
+const playButton = document.querySelector(".modal .play-button");
+let duration = 800;
+
+playButton.addEventListener("click", () => {
+  const username = modalInput.value;
+  if (username.trim() !== "") {
+    document.querySelector(".info-container span").innerHTML =
+      username;
+    modal.remove();
+  } else {
+    alert("Please enter your name or click on close button");
+  }
+});
+
+closeModal.addEventListener("click", () => {
+  modal.remove();
+});
+
+allBlocks.forEach((block) => {
+  block.style.order = Math.floor(Math.random() * 16); // 0-15
+});
+
+allBlocks.forEach((block) => {
+  block.addEventListener("click", () => {
+    block.classList.toggle("active");
+    const activeBlocks = document.querySelectorAll(".game .game-block.active");
+    if (activeBlocks.length === 2) {
+      gameContainer.style.pointerEvents = "none";
+      setTimeout(() => {
+        gameContainer.style.pointerEvents = "auto";
+      }, duration);
+      const [firstBlock, secondBlock] = activeBlocks;
+      if (firstBlock.dataset.tech === secondBlock.dataset.tech) {
+        setTimeout(() => {
+          firstBlock.classList.remove("active");
+          secondBlock.classList.remove("active");
+          firstBlock.classList.add("done");
+          secondBlock.classList.add("done");
+        }, duration);
+      } else {
+        setTimeout(() => {
+          wrongTries.innerHTML = parseInt(wrongTries.innerHTML) + 1;
+          firstBlock.classList.remove("active");
+          secondBlock.classList.remove("active");
+        }, duration);
+      }
+    }
+  });
+});
